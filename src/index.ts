2fe5ba640b43a179568ac825f54516d3ea5b241f
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-08 13:34:41 +0700

    add: context in routing

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia()
+  // Response 
   .get("/", () => "Hello Elysia")
-  .get('/hello', () => 'Hi')
+  .get("/string", () => new Response("hi"))
+
+  // Paths and Parameters
+  .get('/hello', ({ path }) => 'Hi you are in ' + path)
   .get('/id/:id', ({ params : { id } })=> id)
+  .get('/id/:id/:name', ({ params : { id, name }}) => id + ' ' + name)
+  .get('/id/*', ({ params }) => params['*'] )
+  // Custom Method
   .route('M-SEARCH', '/m-search', () => 'connect')
+
   .onError(({ code }) => {
-    if (code === 'NOT_FOUND')
-        return 'Route not found :('
+      if (code === 'NOT_FOUND')
+            return 'Route not found :('
+      })
+
+  // Context set.status and error handling
+  .get('/context' , ({ error }) => error(418, "kirifuji nagisa"))
+  .get('/status' , ({ set }) => {
+    set.status = 418
+    set.headers['x-powered-by'] = 'Elysia'
+
+    return "a mimir"
   })
-  
+  // Redirection to another resource
+  .get('/youtube' , ({ set }) => {
+    set.redirect = 'https://youtu.be/whpVWVWBW4U?&t=8'
+  })
+    // Default error handling
+  .onBeforeHandle(({ set }) => {
+      set.status = 404
+      return "Not Found :("
+    })
+
+
   .listen(3000);
 
 console.log(
